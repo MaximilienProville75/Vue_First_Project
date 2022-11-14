e80439a47f809226a11c865518ec600eaf8b221e
mproville <mproville@etiskapp.com>
2022-11-14 19:54:28 +0100

    GITHUB-SET-UP-DONE

--- src/App.vue
+++ src/App.vue
@@ -10,10 +10,3 @@ export default {
 </script>
 
 <style></style>
-
-<!-- git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin git@github.com:MaximilienProville75/Vue_First_Project.git
-git push -u origin main -->
